dd1f740d9980a72b309b2c302b3adcac99e838f8
Thibault Caron <thibault.caronvans@laposte.net>
2025-01-06 12:54:00 +0100

    added comments to clock.py

--- clock.py
+++ clock.py
@@ -1,15 +1,23 @@
 import time
 
-
 def display_time():
     while True:
+        # initialize time variable (from local time)
         current_time = time.localtime()
+
+        # format time as: hh:mm:ss
         formatted_time = time.strftime("%H:%M:%S", current_time)
+
         print (formatted_time, end="", flush=True)
+
+        # carriage return to replace previous line with new one
         print("\r", end="", flush=True)
+
+        # wait 1 second before each loop
         time.sleep(1)
 
 
 # def set_alarm():
 
+# Run the real-time clock:
 display_time()
