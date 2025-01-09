4d83d04fd74815b12153db09198e4ca0094f51a4
Thibault Caron <thibault.caronvans@laposte.net>
2025-01-09 15:14:02 +0100

    bug: fix some bugs

--- clock.py
+++ clock.py
@@ -199,10 +199,8 @@ if __name__ == "__main__":
 
     # alarm_time = datetime.datetime(datetime.datetime.now().year, datetime.datetime.now().month, datetime.datetime.now().day, 00, 00, 00)
 
-    alarm_time = (00, 00, 00)
-    user_time = (00, 00, 00)
-    time_format = "24h"
-    clock_pause = False
+    # time_format = "24h"
+    # clock_pause = False
 
     clock()
 
